82532b50d3124ed827e1d201be59f2ca814f2165
sinistro14 <tiago14_ribeiro@hotmail.com>
2022-12-19 19:04:00 +0000

    Make music queue thread safe

--- pipo/music/music_queue.py
+++ pipo/music/music_queue.py
@@ -1,4 +1,5 @@
 import random
+from threading import Lock
 from typing import List
 
 from pipo.music.component import Component
@@ -6,41 +7,56 @@ from pipo.music.music import Music
 
 
 class MusicQueue(Component):
-
+    _mutex: Lock
     _children: List[Component]
 
     def __init__(self, children: List[Component] = None) -> None:
         self._children = children or []
+        self._mutex = Lock()
 
     def add(self, component: Component) -> None:
-        self._children.append(component)
+        with self._mutex:
+            self._children.append(component)
         component.parent = self
 
     def pop(self) -> Music:
+        """_summary_
+
+        _extended_summary_
+        Not thread safe if this method is called concurrently.
+
+        Returns
+        -------
+        Music
+            _description_
+        """
+        music = None
         if self.count():
             element = self._children.pop()
-            element.parent = None
             if element.count() > 1:  # meaning we got a queue
                 self._children.insert(0, element)
-            return element.pop()
-        return None
+                element = element.pop() # get first element from queue
+            element.parent = None
+            music = element.pop()
+        return music
 
     @classmethod
     def is_composite(cls) -> bool:
         return True
 
     def clear(self) -> None:
-        for child in self._children:
-            child.clear()
-        self._children = []
+        with self._mutex:
+            self._children = []
 
     def count(self) -> int:
-        return sum([child.count() for child in self._children])
+        with self._mutex:
+            return sum([child.count() for child in self._children])
 
     def shuffle(self) -> None:
-        for child in self._children:
-            child.shuffle()
-        random.shuffle(self._children)
+        with self._mutex:
+            for child in self._children:
+                child.shuffle()
+            random.shuffle(self._children)
 
     def skip_list(self) -> None:
         if self._children[0].is_composite():
