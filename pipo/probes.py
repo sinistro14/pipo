28b35be5e5a5080eb6ce5b0435b5d256d6317536
Tiago Gonçalves <tiago14_ribeiro@hotmail.com>
2024-10-20 19:55:44 +0000

    feat: Add health probes

---
+++ pipo/probes.py
@@ -0,0 +1,44 @@
+import time
+from fastapi import FastAPI
+import contextlib
+import time
+import threading
+import uvicorn
+
+probe_server = FastAPI()
+
+
+class ProbeServer(uvicorn.Server):
+    def install_signal_handlers(self):
+        pass
+
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started and thread.is_alive():
+                time.sleep(1e-3)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
+
+
+def get_probe_server(port: int, log_level: str) -> uvicorn.Server:
+    config = uvicorn.Config(
+        probe_server,
+        port=port,
+        log_level=log_level,
+    )
+    return ProbeServer(config)
+
+
+@probe_server.get("/healthz")
+async def healthiness() -> bool:
+    return True
+
+
+@probe_server.get("/readyz")
+async def readiness() -> bool:
+    return True
